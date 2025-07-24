61b3f251158c4f34b5bf1561ed3e7f34ff086ac8
DimaV11 <dmitriy_vostroknutov@mail.ru>
2025-07-24 18:44:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-CLIENT_SECRET = "Секретный ключ из Нейросети"  # Authorization Key
+CLIENT_SECRET = "Секретный ключ из Нейросети"
 TOKEN = "Ваш Токен"
-MODEL = "Модель ИИ"  # Используем GigaChat, так как GigaChat-2 не поддерживается
+MODEL = "Модель ИИ"
